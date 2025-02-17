26785efac26b55ad4741a953cc30d68eeff55ed5
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-02-17 23:07:40 +0000

    Feat: Add font load

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import { View, ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
+
+  const [loaded] = useFonts({
+    "Anton-Regular": require("../assets/fonts/Anton-Regular.ttf"),
+    "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Light": require("../assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Italic": require("../assets/fonts/Roboto-Italic.ttf"),
+    "Roboto-Semibold": require("../assets/fonts/Roboto-SemiBold.ttf"),
+  })
+
+  if (!loaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#1E90FF" />
+      </View>
+    );
+  }
+
   return (
     <>
       <StatusBar style="dark" />
@@ -9,13 +29,13 @@ export default function RootLayout() {
         <Stack.Screen
           name="(tabs)"
           options={{
-            headerShown: false,
+        headerShown: false,
           }}
         />
         <Stack.Screen
           name="+not-found"
           options={{
-            title: "Not Found",
+        title: "Not Found",
           }}
         />
       </Stack>
