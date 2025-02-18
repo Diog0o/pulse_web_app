5768f5d189f7e3fb66ba3b54cb466c388b88cf61
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-02-18 12:59:38 +0000

    Feat: Login Page

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,26 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import InputBox from "@/components/inputBox";
+import { useState } from "react";
+
+const google_img = require("../assets/images/google.png");
+const facebook_img = require("../assets/images/facebook.png");
+const apple_img = require("../assets/images/apple.png");
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+  const handlePasswordChange = (text: string, validationState?: any) => {
+    setPassword(text);
+    setIsPasswordValid(
+      validationState.min8 &&
+        validationState.min1Upper &&
+        validationState.min1Number &&
+        validationState.min1Special
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View>
@@ -14,13 +33,35 @@ export default function Login() {
         <Text style={styles.signinmessage}>Sign in into your account</Text>
       </View>
       <View style={{ marginBottom: 10 }}>
-        <InputBox title="Username" type="username" />
-        <InputBox title="Password" type="password" />
+        <InputBox
+          title="Username"
+          type="username"
+          onChangeText={(text) => setUsername(text)}
+        />
+        <InputBox
+          title="Password"
+          type="password"
+          onChangeText={handlePasswordChange}
+        />
       </View>
       <View style={styles.separatorcontainer}>
         <View style={styles.orline} />
         <Text style={styles.ortext}>or</Text>
         <View style={styles.orline} />
+      </View>
+      <View>
+        <Text style={styles.undertext}>Sign up usign</Text>
+      </View>
+      <View style={styles.imagecontainers}>
+        <View style={styles.imagecontainer}>
+          <Image source={google_img} style={styles.image} />
+        </View>
+        <View style={styles.imagecontainer}>
+          <Image source={facebook_img} style={styles.image} />
+        </View>
+        <View style={styles.imagecontainer}>
+          <Image source={apple_img} style={styles.image} />
+        </View>
       </View>
     </View>
   );
@@ -28,7 +69,7 @@ export default function Login() {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 150,
+    marginTop: 120,
     alignItems: "center",
   },
 
@@ -52,6 +93,7 @@ const styles = StyleSheet.create({
   separatorcontainer: {
     flexDirection: "row",
     alignItems: "center",
+    marginBottom: 10,
   },
 
   orline: {
@@ -65,5 +107,30 @@ const styles = StyleSheet.create({
     fontFamily: "Roboto-Bold",
     color: "#9F9D9D",
     marginHorizontal: 10,
+  },
+
+  undertext: {
+    fontSize: 12,
+    fontFamily: "Roboto-Regular",
+    color: "#9F9D9D",
+  },
+
+  imagecontainers: {
+    flexDirection: "row",
+    width: 200,
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+
+  imagecontainer: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "#9F9D9D",
+    padding: 7,
+  },
+
+  image: {
+    width: 20,
+    height: 20,
   },
 });
